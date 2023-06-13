307958d6d976f6bb852d63fa895e72c229666f17
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-06-13 10:45:51 -0700

    Added loader and error handling.

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,21 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './Search.css';
 import { fetchBookByName } from '../../API/API';
 import type { RootState } from '../../redux/store';
 import { useSelector, useDispatch } from 'react-redux';
 import { setValue, setBooks } from '../../redux/searchSlice';
 import BookItem from '../../components/BookItem/BookItem';
+import { ReactComponent as Spinner } from './Spinner.svg';
 
 const Search: FC = () => {
   const inputValue = useSelector((state: RootState) => state.search.value);
   const books = useSelector((state: RootState) => state.search.books);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setShowErrorMessage] = useState('');
 
   const handleSearch = () => {
-    fetchBookByName(inputValue).then((res) => {
-      console.log(res ? res : []);
-      dispatch(setBooks(res));
-    });
+    setLoading(true);
+    setShowError(false);
+    setShowErrorMessage('');
+
+    fetchBookByName(inputValue)
+      .then((res) => {
+        dispatch(setBooks(res));
+      })
+      .catch((err) => {
+        setShowErrorMessage(err);
+        setShowError(true);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      });
   };
 
   return (
@@ -29,8 +46,11 @@ const Search: FC = () => {
         />
         <button onClick={handleSearch}>Search</button>
       </div>
+      {loading && <Spinner />}
+      {showError && <h2>{errorMessage}</h2>}
       <ul>
         {books.length > 0 &&
+          !loading &&
           books.map((book) => <BookItem key={book.id} book={book} />)}
       </ul>
     </div>
